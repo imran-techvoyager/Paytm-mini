c6160067edec34ae8f54062d4c3f199f166233f3
Imran Ahmed <imran.techvoyager@gmail.com>
2025-06-26 16:05:59 +0530

    feat(auth): implement authentication middleware and user signup/signin routes

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const cors = require('cors');
 
-app.use(cors());
-app.use(express.json()); //this is body parser that lets you parse json objects from the request body
 const mainRouter = require('./routes/index');
 
 const app = express();
+
+app.use(cors());
+app.use(express.json()); //this is body parser that lets you parse json objects from the request body
 
 app.use("/api/v1", mainRouter);
 app.listen(3000, () => {
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,14 +4,15 @@ const zod = require('zod');
 const { User } = require('../db');
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = require('../config');
+const authMiddleware = require('../middleware');
 
 const signUpSchema = zod.object({
-    username: zod.string().min(3, 'Username must be at least 3 characters long'),
+    username: zod.string().min(3, 'Username must be at least 3 characters long').email(),
     password: zod.string().min(6, 'Password must be at least 6 characters long'),
     firstName: zod.string().min(1, 'First name is required'),
     lastName: zod.string().min(1, 'Last name is required'),
 })
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     const body = req.body;
     const { success } = signUpSchema.safeParse(body);
     if(!success){
@@ -20,17 +21,23 @@ router.post('/signup', (req, res) => {
         });
     }
 
-    const user = User.findOne({
-        username: body.username
+    const existingUser = await User.findOne({
+        username: req.body.username
     });
 
-    if(user._id){
-        return res.json({
+    if(existingUser){
+        return res.status(411).json({
             message: 'User already exists'
         })
     }
 
-    const dbUser = User.create(body);
+    const dbUser = await User.create({
+        username: req.body.username,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
+    });
+
     const token = jwt.sign({
         userId: dbUser._id
     }, JWT_SECRET);
@@ -43,6 +50,90 @@ router.post('/signup', (req, res) => {
     return res.json({
         message: 'User created successfully',
         token: token
+    });
+});
+
+const signInSchema = zod.object({
+    username: zod.string().min(3, 'Username must be at least 3 characters long').email(),
+    password: zod.string().min(6, 'Password must be at least 6 characters long'),
+})
+
+router.post('/signin', async (req, res) => {
+   const body = req.body;
+   const { success } = signInSchema.safeParse(body);
+   if(!success){
+    res.status(411).json({
+        message: "incorrect input"
+    });
+   }
+
+   const user = await User.findOne({
+    username: req.body.username,
+    password: req.body.password
+   });
+   if(user){
+      const token = jwt.sign({
+         userId: user._id
+      }, JWT_SECRET);
+
+      res.json({
+        token: token
+      });
+
+      return;
+   }
+
+   res.status(411).json({
+     message: "error logging in"
+   });
+});
+
+const updateScheme = zod.object({
+    password: zod.string(),
+    firstName: zod.string(),
+    lastName: zod.string()
+});
+
+router.post('/', authMiddleware, async (req, res) => {
+    const body = req.body;
+    const { success } = updateScheme.safeParse(body);
+    if(!success){
+        res.status(411).json({
+            message: "error while updating information"
+        });
+    }
+
+    await User.updateOne(req.body, {
+        id: req.userId
+    })
+
+    res.json({
+        message: "updated successfully"
+    });
+});
+
+router.get('/bulk', async (req, res) => {
+    const filter = req.query.filter || "";
+
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    });
+
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
     });
 });
 
